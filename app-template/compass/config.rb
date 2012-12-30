68ec615c7a302917eb4bf98ef146341d8e058521
Matthew Sherborne <msherborne@gmail.com>
2012-12-30 12:02:10 +1000

    Start of the app-template

---
+++ app-template/compass/config.rb
@@ -0,0 +1,9 @@
+# Require any additional compass plugins here.
+# Set this to the root of your project when deployed:
+http_path = "/"
+sass_dir = "src"
+images_dir = "images"
+javascripts_dir = "javascripts"
+css_dir = "../../qtcreator-build/exampleApp/static-content/resources/themes/default"
+output_style = :extended
+relative_assets = true
